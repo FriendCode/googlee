ff866241490bb966c8e3a6d4a1d16bf4cbaadfe7
Aaron O'Mullan <aaron.omullan@friendco.de>
2012-10-30 02:07:42 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Requires
+var fetcher = require('./lib/fetcher');
+var server = require('./lib/server');
+
+
+// Nothing
+
+// Exports
+module.exports.fetchPage = fetcher.fetchPage;
+module.exports.createServer = server.createServer;
